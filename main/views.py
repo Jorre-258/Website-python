9b58569b68d3a407f931ab76157d898f61fe3b08
jorre18 <delcroixjorre@gmail.com>
2023-05-07 20:11:23 +0200

    Website

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+from .forms import Create_New_List
+# Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {} )
+
+def create(response):
+    form = Create_New_List()
+    return render(response, "main/create.html", {"form":form})
+
